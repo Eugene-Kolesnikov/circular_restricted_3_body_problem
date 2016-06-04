1ef5109f1a572515d8e4feabff659dab97b01fc2
Eugene <eugene.kolesnikov.95@gmail.com>
2016-06-04 15:22:55 +0300

    Update

---
+++ trajectoryModel_grt_rtw/trajectoryModel_types.h
@@ -0,0 +1,31 @@
+/*
+ * trajectoryModel_types.h
+ *
+ * Code generation for model "trajectoryModel".
+ *
+ * Model version              : 1.84
+ * Simulink Coder version : 8.7 (R2014b) 08-Sep-2014
+ * C source code generated on : Sun May 29 20:16:58 2016
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+#ifndef RTW_HEADER_trajectoryModel_types_h_
+#define RTW_HEADER_trajectoryModel_types_h_
+#include "rtwtypes.h"
+#include "builtin_typeid_types.h"
+#include "multiword_types.h"
+
+/* Parameters for system: '<Root>/If Action Subsystem' */
+typedef struct P_IfActionSubsystem_trajector_T_ P_IfActionSubsystem_trajector_T;
+
+/* Parameters (auto storage) */
+typedef struct P_trajectoryModel_T_ P_trajectoryModel_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_trajectoryModel_T RT_MODEL_trajectoryModel_T;
+
+#endif                                 /* RTW_HEADER_trajectoryModel_types_h_ */
